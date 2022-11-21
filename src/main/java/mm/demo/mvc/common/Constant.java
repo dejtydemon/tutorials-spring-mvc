bd8f4ec31954b70cebb2f2f961ba75a73397d609
anhlt@etc <anhlt>
2022-11-21 17:56:34 +0700

    add home controller

---
+++ src/main/java/mm/demo/mvc/common/Constant.java
@@ -0,0 +1,12 @@
+package mm.demo.mvc.common;
+
+public class Constant {
+	
+	public static final class HomeRoute{
+		public static final String DEFAULT = "/";
+		
+		
+	}
+	
+	
+}
--- src/main/java/mm/demo/mvc/web/home/HomeController.java
+++ src/main/java/mm/demo/mvc/web/home/HomeController.java
@@ -1,16 +1,18 @@
-package mm.demo.mvc.web;
+package mm.demo.mvc.web.home;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import mm.demo.mvc.common.Constant;
+
 @Controller
-public class SimpleController {
+public class HomeController {
 
     @Value("${spring.application.name}")
     String appName;
-
-    @RequestMapping("/")
+    
+    @RequestMapping(Constant.HomeRoute.DEFAULT)
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
         model.addAttribute("test", "hello babe");
